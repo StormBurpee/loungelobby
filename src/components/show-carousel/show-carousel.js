72488bce30c92552b4632df8951d2d80aa4b5697
Storm Burpee <stormburpee@gmail.com>
2018-04-12 22:25:21 +0930

    further work on show carousel

--- src/components/show-carousel/show-carousel.js
+++ src/components/show-carousel/show-carousel.js
@@ -3,6 +3,15 @@ import './show-carousel.scss';
 import { Carousel } from 'antd';
 
 class ShowCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {shows: []};
+    if(props.shows != null){
+      this.state.shows = props.shows;
+      console.log(props.shows);
+    }
+  }
+
   render() {
     return (
       <div className="show-carousel">
--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import ShowDisplay from '../../components/show-display/show-display';
+import ShowCarousel from '../../components/show-carousel/show-carousel';
 
 class Home extends Component {
   render() {
     return (
       <div className="page-home">
         <div className="ll-section featured">
-
+          <ShowCarousel shows={[{name: "show1"}, {name: "show2"}]} />
         </div>
         <div className="page">
           <div className="ll-section continue-watching">
-            <div class="section-title">Continue Watching</div>
+            <div className="section-title">Continue Watching</div>
           </div>
           <div className="ll-section for-you">
-            <div class="section-title">For You</div>
+            <div className="section-title">For You</div>
           </div>
           <div className="ll-section popular">
-            <div class="section-title">Popular Now</div>
+            <div className="section-title">Popular Now</div>
           </div>
         </div>
       </div>
