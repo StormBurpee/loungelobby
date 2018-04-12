42457b614cc80892cefeb91bed288d32c279a477
Storm Burpee <stormburpee@gmail.com>
2018-04-12 21:59:11 +0930

    Basic header done, starting show display

---
+++ src/components/show-display/show-display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './show-display.scss';
+
+class ShowDisplay extends Component {
+  render() {
+    return (
+      <div className="show-display">
+      </div>
+    );
+  }
+}
+
+export default ShowDisplay;
