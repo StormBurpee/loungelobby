52b61fd6b433d39f2f7e0444de5a343cf402bf1f
Storm Burpee <stormburpee@gmail.com>
2018-04-12 22:11:15 +0930

    blocking out home page + show carousel and display

---
+++ src/components/show-carousel/show-carousel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './show-carousel.scss';
+import { Carousel } from 'antd';
+
+class ShowCarousel extends Component {
+  render() {
+    return (
+      <div className="show-carousel">
+      </div>
+    );
+  }
+}
+
+export default ShowCarousel;
--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import ShowDisplay from '../../components/show-display/show-display';
 
 class Home extends Component {
   render() {
     return (
       <div className="page-home">
+        <div className="ll-section featured">
+
+        </div>
+        <div className="page">
+          <div className="ll-section continue-watching">
+            <div class="section-title">Continue Watching</div>
+          </div>
+          <div className="ll-section for-you">
+            <div class="section-title">For You</div>
+          </div>
+          <div className="ll-section popular">
+            <div class="section-title">Popular Now</div>
+          </div>
+        </div>
       </div>
     );
   }
